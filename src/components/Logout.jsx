7167a68d96f5f4bceff67e958a797da1e6d590fb
Torben Bis <bis.torben@gmail.com>
2024-02-26 10:57:10 +0100

    add logout styling: final MVP auth process

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,9 +1,16 @@
 import axios from "axios";
+import "./Logout.css";
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext.jsx";
+import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
-  const { setLoggedIn } = useContext(AuthContext);
+  const { loggedIn, setLoggedIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  if (!loggedIn) {
+    navigate("/");
+  }
   const logoutHandler = async () => {
     try {
       const response = await axios.post(
@@ -14,17 +21,21 @@ const Logout = () => {
         }
       );
       setLoggedIn(false);
+
       console.log(response.data);
     } catch (err) {
       console.log(err);
     }
   };
   return (
-    <>
-      <h1>LOGOUT</h1>
-      <p>Wollen sie sich ausloggen?</p>
-      <button onClick={logoutHandler}>Logout</button>
-    </>
+    <section className="Logout wrapper-form-section">
+      <h1>LOG OUT</h1>
+      <p>Are you sure, you want to log out?</p>
+
+      <button onClick={logoutHandler} className="btn-valid">
+        LOG OUT
+      </button>
+    </section>
   );
 };
 
